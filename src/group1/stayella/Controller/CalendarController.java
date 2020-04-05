4f97ddf0d7564c46fb3007fb64f63496d5d4351a
version-1 <j.onodera26@gmail.com>
2020-04-05 10:30:41 -0700

    add buttons

--- src/group1/stayella/Controller/CalendarController.java
+++ src/group1/stayella/Controller/CalendarController.java
@@ -3,10 +3,23 @@ package group1.stayella.Controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class CalendarController extends ApplicationController {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("Calender Controller");
+    }
+
+    @FXML
+    public void onClickVacancy(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void onClickOccupied(ActionEvent event) {
+
     }
 }
