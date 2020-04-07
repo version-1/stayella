6142e97d824fc2df3611a54316a39034b31f8203
version-1 <j.onodera26@gmail.com>
2020-04-07 02:40:07 -0700

    cd

--- src/group1/stayella/Controller/CalendarController.java
+++ src/group1/stayella/Controller/CalendarController.java
@@ -20,10 +20,26 @@ public class CalendarController extends ApplicationController {
 
     public void initTable() {
 
-        TableColumn<Room, ?> col1 = new TableColumn<>("Room No");
+        TableColumn<Room, ?> col1 = new TableColumn<>("Room");
         col1.setCellValueFactory(new PropertyValueFactory<>("roomNumber"));
 
+        TableColumn<Room, ?> col2 = new TableColumn<>("Price");
+        col2.setCellValueFactory(new PropertyValueFactory<>("roomPrice"));
+
+        TableColumn<Room, ?> col3 = new TableColumn<>("Capacity");
+        col3.setCellValueFactory(new PropertyValueFactory<>("roomCapacity"));
+
+        TableColumn<Room, ?> col4 = new TableColumn<>("Smoke");
+        TableColumn<Room, ?> col5 = new TableColumn<>("Pet");
+        TableColumn<Room, ?> col6 = new TableColumn<>("Status");
+        // col3.setCellValueFactory(new PropertyValueFactory<>("roomCapacity"));
+
         calendar.getColumns().add(col1);
+        calendar.getColumns().add(col2);
+        calendar.getColumns().add(col3);
+        calendar.getColumns().add(col4);
+        calendar.getColumns().add(col5);
+        calendar.getColumns().add(col6);
 
         for (Room room : getRooms()) {
             calendar.getItems().add(room);
