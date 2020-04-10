9be77a86325076582faf1d84c4344d368c4d5b52
Tomona Sako <sakotomona@gmail.com>
2020-04-10 14:20:13 -0700

    Made table columns value

--- src/group1/stayella/Controller/ReservationListController.java
+++ src/group1/stayella/Controller/ReservationListController.java
@@ -1,15 +1,20 @@
 package group1.stayella.Controller;
 
 
+import group1.stayella.Model.Guest;
 import group1.stayella.Model.Reservation;
 import group1.stayella.Model.Room;
 import group1.stayella.Model.Vacancy;
 import group1.stayella.View.ReservationListView.ReservationList;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import group1.stayella.Resources.Images.Icon;
 
+import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -20,127 +25,139 @@ public class ReservationListController extends ApplicationController {
     @FXML
     public Label title;
     @FXML
-    public Button reload;
+    public Button edit;
     @FXML
     public TableView table;
+    @FXML
+    public Button back;
 
-    private ObservableList<ReservationList> data = FXCollections.observableArrayList();
+
+    private ObservableList<ReservationList> data;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        this.data = FXCollections.observableArrayList();
+        setDataList();
         setTable();
     }
 
     public void setTable() {
-        table.setEditable(false);
+        this.table.setEditable(false);
 
-        TableColumn reservationInfo = new TableColumn("Reservation");
-        TableColumn guestInfo = new TableColumn("Guest Information");
-
-        TableColumn reservationNoCol = new TableColumn("Reservation No.");
-        TableColumn statusCol = new TableColumn("Status");
-        TableColumn checkInCol = new TableColumn("Check In");
-        TableColumn checkOutCol = new TableColumn("Check Out");
-        TableColumn roomNumberCol = new TableColumn("Room Number");
-        TableColumn roomAdditionCol = new TableColumn("Room Addition");
-
-        TableColumn guestNameCol = new TableColumn("Guest Name");
-        TableColumn numberOfGuestsCol = new TableColumn("Number Of Guests");
-        TableColumn ageCol = new TableColumn("Age");
-        TableColumn languageCol = new TableColumn("Language");
-        TableColumn eMailCol = new TableColumn("E-mail");
-        TableColumn phoneCol = new TableColumn("Phone");
-        TableColumn homeAddressCol = new TableColumn("Home Address");
+        table.setRowFactory(tv -> {
+            TableRow<ReservationList> row = new TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 2 && (! row.isEmpty()) ) {
+                    String reservationNo = row.getItem().getReservationNo();
+                    System.out.println(reservationNo);
+                }
+            });
+            return row ;
+        });
 
 
-        reservationInfo.getColumns().addAll(reservationNoCol, statusCol, checkInCol, checkOutCol, roomNumberCol, roomAdditionCol);
-        guestInfo.getColumns().addAll(guestNameCol, numberOfGuestsCol, ageCol, languageCol, eMailCol, phoneCol, homeAddressCol);
+        TableColumn<Reservation, String> reservationNoCol = new TableColumn<>("Reservation No.");
+        TableColumn<Guest, String> guestNameCol = new TableColumn<>("Guest Name");
+        TableColumn<Reservation, Integer> numberOfGuestsCol = new TableColumn<>("Number Of Guests");
+        TableColumn<Reservation, String> statusCol = new TableColumn<>("Status");
+        TableColumn<Reservation, String> checkInCol = new TableColumn<>("Check In");
+        TableColumn<Reservation, String> checkOutCol = new TableColumn<>("Check Out");
+        TableColumn<Reservation, String> roomNumberCol = new TableColumn<>("Room Number");
+        TableColumn<Reservation, Integer> roomAdditionCol = new TableColumn<>("Room Addition");
 
-        table.getColumns().addAll(reservationInfo, guestInfo);
+        table.getColumns().addAll(reservationNoCol, guestNameCol, numberOfGuestsCol, statusCol, checkInCol, checkOutCol, roomNumberCol, roomAdditionCol);
 
+        this.table.itemsProperty().setValue(data);
+        this.table.setItems(data);
+        reservationNoCol.setCellValueFactory(new PropertyValueFactory<>("reservationNo"));
+        statusCol.setCellValueFactory(new PropertyValueFactory<>("status"));
+        checkInCol.setCellValueFactory(new PropertyValueFactory<>("checkIn"));
+        checkOutCol.setCellValueFactory(new PropertyValueFactory<>("checkOut"));
+        roomNumberCol.setCellValueFactory(new PropertyValueFactory<>("roomNumber"));
+        roomAdditionCol.setCellValueFactory(new PropertyValueFactory<>("roomAddition"));
+        guestNameCol.setCellValueFactory(new PropertyValueFactory<>("guestName"));
+        numberOfGuestsCol.setCellValueFactory(new PropertyValueFactory<>("numberOfGuests"));
 
-        setDataList();
-
-//        reservationNoCol.setCellValueFactory(
-//                new PropertyValueFactory<>("reservationNo")
-//        );
-//        statusCol.setCellValueFactory(
-//                new PropertyValueFactory<>("status")
-//        );
-//        checkInCol.setCellValueFactory(
-//                new PropertyValueFactory<>("checkIn")
-//        );
     }
 
     // make list
-    private int getNumberOfList() {
-        ArrayList<Room> rooms = (ArrayList<Room>) getHotel().getRooms();
-        int NumberOfRoom = rooms.size();
-        int NumberOfVacancy = 0;
-        for (int i = 0; i < NumberOfRoom; i++) {
-            NumberOfVacancy += rooms.get(i).getVacancies().size();
+    private boolean checkSameReservation(int reservationId, ObservableList<ReservationList> data) {
+        boolean answer = true;
+        for (int i = 0; i < data.size(); i++) {
+            if (data.get(i).getReservationId() == reservationId) {
+                answer = false;
+                break;
+            }
         }
-        return NumberOfVacancy;
-    }
-
-    private boolean checkSameReservation(Reservation reservation1, Reservation reservation2){
-        if(reservation1.equals(reservation2)) {
-            return false;
-        } else {
-            return true;
-        }
+        return answer;
     }
 
     private void setDataList() {
         for (int i = 0; i < getHotel().getRooms().size(); i++) {
             Room room = getHotel().getRooms().get(i);
-            Reservation compared;
+
             for (int k = 0; k < room.getVacancies().size(); k++) {
-                System.out.println("Room:" + i + ", vacancy: " + k);
                 Reservation reservation = room.getVacancies().get(k).getReservation();
-                if (reservation != null){
+                if (reservation != null) {
+                    int reservationId = reservation.getId();
                     String reservationNo = reservation.getReservationNo();
-                    int status = reservation.getStatus();
-                    LocalDate checkIn = reservation.getCheckInTime();
-                    LocalDate checkOut = reservation.getCheckOutTime();
-                    String roomNumber = room.getRoomNumber();
-                    int roomAddition = reservation.getCharges().size();
                     String guestName = reservation.getMainGuest().getName();
                     int numberOfGuests = reservation.getNumberOfGuest();
-                    String age = reservation.getMainGuest().getAge();
-                    String language = reservation.getMainGuest().getLanguage();
-                    String eMail = reservation.getMainGuest().getEmailAddress();
-                    String phone = reservation.getMainGuest().getPhoneNumber();
-//                String homeAddress = reservation.getMainGuest().get
-                    System.out.println(reservationNo+" " + status+" " +
-                            checkIn+" " +
-                            checkOut+" " +
-                            roomNumber+" " +
-                            roomAddition+" " +
-                            guestName+" " +
-                            numberOfGuests+" " +
-                            age+" " +
-                            language+" " +
-                            eMail+" " + phone);
-//                    data.add(
-//                            new ReservationList(
-//                                    reservationNo, status,
-//                                    checkIn,
-//                                    checkOut,
-//                                    roomNumber,
-//                                    roomAddition,
-//                                    guestName,
-//                                    numberOfGuests,
-//                                    age,
-//                                    language,
-//                                    eMail, phone
-//                            )
-//                    );
+                    int status = reservation.getStatus();
+//                    String checkIn = reservation.getCheckOutTime().toString();
+//                    String checkOut = reservation.getCheckInTime().toString();
+
+                    String checkIn = "Check In";
+                    String checkOut = "Check Out";
+
+                    String roomNumber = room.getRoomNumber();
+                    int roomAddition = reservation.getCharges().size();
+
+                    ReservationList list = new ReservationList(
+                            reservationId,
+                            reservationNo,
+                            guestName,
+                            numberOfGuests,
+                            status,
+                            checkIn,
+                            checkOut,
+                            roomNumber,
+                            roomAddition
+                    );
+
+                    System.out.println(
+                            room.getID() + " " +
+                            room.getNumberOfBeds()+ " " +
+                            reservationNo+ " " +
+                            guestName+ " " +
+                            numberOfGuests+ " " +
+                            status+ " " +
+                            checkIn+ " " +
+                            checkOut+ " " +
+                            roomNumber+ " " +
+                            roomAddition
+
+                    );
+
+
+                    if (checkSameReservation(reservationId, data)) {
+                        data.add(list);
+                    }
                 }
             }
 
         }
     }
 
+    @FXML
+    public void transitToHome(ActionEvent actionEvent) throws IOException {
+        transitTo(actionEvent, "index.fxml", 500, 500);
+    }
+
+
+    @FXML
+    public void transitToReservationList(ActionEvent actionEvent) throws IOException {
+        transitTo(actionEvent, "ReservationListView/index.fxml", 800, 600);
+    }
 
 }
--- src/group1/stayella/View/ReservationListView/ReservationList.java
+++ src/group1/stayella/View/ReservationListView/ReservationList.java
@@ -12,7 +12,7 @@ public class ReservationList {
     private final static int UNCONFIRMED = 1;
     private final static int CONFIRMED = 2;
 
-
+    private final SimpleIntegerProperty reservationId;
     private final SimpleStringProperty reservationNo;
     private final SimpleStringProperty status;
     private final SimpleStringProperty checkIn;
@@ -22,45 +22,34 @@ public class ReservationList {
 
     private final SimpleStringProperty guestName;
     private final SimpleIntegerProperty numberOfGuests;
-    private final SimpleStringProperty age;
-    private final SimpleStringProperty language;
-    private final SimpleStringProperty eMail;
-    private final SimpleStringProperty phone;
-//    private final SimpleStringProperty homeAddress;
 
 
-    public ReservationList (
+    public ReservationList(
+            int reservationId,
             String reservationNo,
-            int status,
-            LocalDate checkIn,
-            LocalDate checkOut,
-            String roomNumber,
-            int roomAddition,
             String guestName,
             int numberOfGuests,
-            String age,
-            String language,
-            String eMail,
-            String phone
-//            ,
-//            String homeAddress
+            int status,
+            String checkIn,
+            String checkOut,
+            String roomNumber,
+            int roomAddition
     ) {
+        this.reservationId = new SimpleIntegerProperty(reservationId);
         this.reservationNo = new SimpleStringProperty(reservationNo);
+        this.guestName = new SimpleStringProperty(guestName);
+        this.numberOfGuests = new SimpleIntegerProperty(numberOfGuests);
         this.status = new SimpleStringProperty(convertStatus(status));
-        this.checkIn = new SimpleStringProperty(checkIn.toString());
-        this.checkOut = new SimpleStringProperty(checkOut.toString());
+        this.checkIn = new SimpleStringProperty(checkIn);
+        this.checkOut = new SimpleStringProperty(checkOut);
         this.roomNumber = new SimpleStringProperty(roomNumber);
         this.roomAddition = new SimpleIntegerProperty(roomAddition);
 
-        this.guestName = new SimpleStringProperty(guestName);
-        this.numberOfGuests = new SimpleIntegerProperty(numberOfGuests);
-        this.age = new SimpleStringProperty(age);
-        this.language = new SimpleStringProperty(language);
-        this.eMail = new SimpleStringProperty(eMail);
-        this.phone = new SimpleStringProperty(phone);
-//        this.homeAddress = new SimpleStringProperty(homeAddress);
     }
 
+    public int getReservationId() {
+        return reservationId.get();
+    }
 
     public String getReservationNo() {
         return reservationNo.get();
@@ -94,28 +83,11 @@ public class ReservationList {
         return numberOfGuests.get();
     }
 
-    public String getAge() {
-        return age.get();
-    }
-
-    public String getLanguage() {
-        return language.get();
-    }
-
-    public String getEMail() {
-        return eMail.get();
-    }
-
-    public String getPhone() {
-        return phone.get();
-    }
-
-//    public String getHomeAddress() {
-//        return homeAddress.get();
-//    }
 
     // setter
-
+    public void setReservationId(int reservationId) {
+        this.reservationId.set(reservationId);
+    }
 
     public void setReservationNo(String reservationNo) {
         this.reservationNo.set(reservationNo);
@@ -149,32 +121,13 @@ public class ReservationList {
         this.numberOfGuests.set(numberOfGuests);
     }
 
-    public void setAge(String age) {
-        this.age.set(age);
-    }
-
-    public void setLanguage(String language) {
-        this.language.set(language);
-    }
-
-    public void setEMail(String eMail) {
-        this.eMail.set(eMail);
-    }
-
-    public void setPhone(String phone) {
-        this.phone.set(phone);
-    }
-
-//    public void setHomeAddress(String homeAddress) {
-//        this.homeAddress.set(homeAddress);
-//    }
 
     //
 
     //method
     static String convertStatus(int status) {
         String answer;
-        switch(status) {
+        switch (status) {
             case CANCEL:
                 answer = "Cancel";
                 break;
