469264f249b909b207c1c593b5b1d6ce86292ac6
version-1 <j.onodera26@gmail.com>
2020-04-11 11:37:42 -0700

    fix icon

--- src/group1/stayella/Resources/Images/Icon.java
+++ src/group1/stayella/Resources/Images/Icon.java
@@ -14,7 +14,7 @@ public class Icon {
   public final static String CLEAN_LIGHT = BASE_PATH + "clean-light.png";
   public final static String DOLLAR = BASE_PATH + "dollar-dark.png";
   public final static String EDIT = BASE_PATH + "edit-dark.png";
-  public final static String PEOPLE = BASE_PATH + "edit-dark.png";
+  public final static String PEOPLE = BASE_PATH + "people-dark.png";
   public final static String PET = BASE_PATH + "pet-dark.png";
   public final static String PET_LIGHT = BASE_PATH + "pet-light.png";
   public final static String RELOAD = BASE_PATH + "reload-dark.png";
