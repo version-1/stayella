4b2ecca81dce8f7ddf5b909ef8b9f1bc780839ce
Tomona Sako <sakotomona@gmail.com>
2020-04-11 21:39:42 -0700

    fix home css

--- src/group1/stayella/Controller/HomeController.java
+++ src/group1/stayella/Controller/HomeController.java
@@ -1,13 +1,23 @@
 package group1.stayella.Controller;
 
+import group1.stayella.Resources.Images.Icon;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -15,28 +25,55 @@ import java.util.ResourceBundle;
 import group1.stayella.Model.Hotel;
 import group1.stayella.Model.CSV.Factory;
 
+import javax.swing.text.Element;
+
 public class HomeController extends ApplicationController {
-    @FXML
-    public Button reservation;
     @FXML
     public Button calendar;
     @FXML
-    public Label label;
+    public Button reservation;
     @FXML
-    public Button roomAdd;
     public Button reservationList;
+    @FXML
+    public Label title;
+    @FXML
+    public Label version;
+
+
+    @FXML
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         List<Hotel> hotels = Factory.initialize();
         setHotel(hotels.get(0));
-
-        label.setText("STAYELLA\nVersion: 1.0");
+        putImagesOnButton();
+        title.setText("STAYELLA");
+        version.setText("STAYELLA Version: 1.0");
     }
 
     @FXML
+    private void putImagesOnButton() {
+        javafx.scene.image.ImageView book = Icon.getWithLayout(Icon.CALENDAR_WHITE, 82, 94);
+        calendar.setText("Booking List");
+        calendar.setGraphic(book);
+        calendar.setContentDisplay(ContentDisplay.TOP);
+        calendar.setTextAlignment(TextAlignment.CENTER);
+
+        javafx.scene.image.ImageView edit = Icon.getWithLayout(Icon.EDIT_WHITE, 56, 50);
+        reservation.setText("New Booking ");
+        reservation.setGraphic(edit);
+
+        javafx.scene.image.ImageView listBook = Icon.getWithLayout(Icon.BOOK_WHITE, 56, 50);
+        reservationList.setText("Booking List");
+        reservationList.setGraphic(listBook);
+
+    }
+
+
+    @FXML
     public void transitToReservation(ActionEvent actionEvent) throws IOException {
-        transitTo(actionEvent, "ReservationView/index.fxml", 690, 800);
+        popUpAs(actionEvent, "ReservationView/index.fxml", 690, 800);
     }
 
     @FXML
